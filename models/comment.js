fdd4be22dba03f29d4e1ed3c828be764a170cacd
Pratik-sys <pithore.pratik@gmail.com>
2021-11-30 16:55:07 +0530

    rewrite the models

--- models/comment.js
+++ models/comment.js
@@ -1,16 +1,22 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-var commentSchema = mongoose.Schema
-    ({
-        text:String,
-        createdAt : {type: Date, default: Date.now},
-        author :{
-            id:{
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"User"
-            },
-            username: String
-        }
-    });
-    
+const commentSchema = mongoose.Schema({
+  text: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: {
+      type: String,
+    },
+  },
+});
+
 module.exports = mongoose.model("Comment", commentSchema);
--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-var UserSchema = new mongoose.Schema ({
-    username: String,
-    password: String
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-UserSchema.plugin(passportLocalMongoose)
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
