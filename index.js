aeb70abcadcca7f409348cd917ec8d2f7a5fe74a
Pratik-sys <pithore.pratik@gmail.com>
2021-11-30 16:40:52 +0530

    use express.urlencoded
    - As express provides built-in middleware function `urlencoded()`, no more need of body-parser

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-const bodyParser = require("body-parser");
 const config = require("./config");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
@@ -16,7 +15,7 @@ require("dotenv").config();
 
 config.DBConfig();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(cookieParser("secret"));
