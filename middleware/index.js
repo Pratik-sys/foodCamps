6e1ff26f313a525a6c4a2f0578b98c2095f8fd3c
Pratik-sys <pithore.pratik@gmail.com>
2021-12-24 15:33:13 +0530

    middleware to authorize user

--- middleware/index.js
+++ middleware/index.js
@@ -6,4 +6,12 @@ module.exports = {
     req.flash("error", "Please Sign in to do that!!");
     res.redirect("/login");
   },
+  authorize: (...roles) => {
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return res.send("<h1>unauthorize user access</h1>");
+      }
+      next();
+    };
+  },
 };
