539f186120395195f020ccfe39c0a181de80e47d
Pratik-sys <pithore.pratik@gmail.com>
2021-12-25 13:29:51 +0530

    admin: ability to delete foodground

--- routes/Admin.js
+++ routes/Admin.js
@@ -4,6 +4,7 @@ const { Foodground } = require("../models");
 const { Comment } = require("../models");
 const { User } = require("../models");
 const middleware = require("../middleware");
+const Cloudinary = require("../utils/Cloudinary");
 
 router.get(
   "/list-alluser",
@@ -22,6 +23,29 @@ router.get(
         foodground: foodground,
         comment: comment,
       });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+router.delete(
+  "/:id/delete",
+  middleware.isLoggedIn,
+  middleware.authorize("admin"),
+  async (req, res) => {
+    try {
+      const foodground = await Foodground.findOne({
+        _id: req.params.id,
+      });
+      if (!foodground) {
+        req.flash("error", foodground.name + "can't be deleted");
+        res.redirect("/foodgrounds");
+      }
+      await Cloudinary.DeleteImage(foodground.image.cloudinary_ID);
+      await foodground.remove({ _id: foodground.id });
+      await Comment.deleteMany({ _id: foodground.comments });
+      req.flash("success", "Foodground deleted!");
+      res.redirect("admin/list");
     } catch (err) {
       console.log(err);
     }
