807ef17491d3b672dd22af10b3b5cb74c9ea53d5
Pratik-sys <pithore.pratik@gmail.com>
2022-01-05 12:52:53 +0530

    cloudinary: let user define the folder name

--- utils/Cloudinary.js
+++ utils/Cloudinary.js
@@ -1,16 +1,16 @@
 const cloudinary = require("cloudinary").v2;
 
-const PreSets = (name) => {
+const PreSets = (name, folder) => {
   return {
-    folder: "foodgrounds",
+    folder: `${folder}`,
     use_filename: true,
     filename_override: `${name}-${new Date().toUTCString()}`,
     unique_filename: false,
   };
 };
 
-module.exports.CloudinaryUpload = async (image, name) => {
-  const result = await cloudinary.uploader.upload(image, PreSets(name));
+module.exports.CloudinaryUpload = async (image, name, folder) => {
+  const result = await cloudinary.uploader.upload(image, PreSets(name, folder));
   return {
     cloudinary_ID: result.public_id,
     path: result.url,
@@ -18,7 +18,7 @@ module.exports.CloudinaryUpload = async (image, name) => {
 };
 
 module.exports.DeleteImage = async (id) => {
-  if (id != process.env.DEFAULT_IMAGE_ID) {
+  if (id != process.env.DEFAULT_IMAGE_ID || process.env.DEFAULT_AVATAR_ID) {
     await cloudinary.uploader.destroy(id);
   }
 };
