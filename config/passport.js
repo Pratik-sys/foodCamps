75b2571ccd5ec8e3a883fa8a24f36009e3a11db9
Pratik-sys <pithore.pratik@gmail.com>
2022-01-08 09:23:22 +0530

    add some check and flash msg if no user found

--- config/passport.js
+++ config/passport.js
@@ -5,15 +5,19 @@ const { User } = require("../models");
 module.exports = (passport) => {
   passport.use(
     new LocalStrategy((username, password, done) => {
-      User.findOne({ name: username })
-        .then(async (user) => {
-          const comparehash = await bcrypt.compare(password, user.password);
-          if (user && comparehash) {
-            return done(null, user);
-          }
-          return done(null, false);
-        })
-        .catch((err) => console.log(err));
+      User.findOne({ name: username }, async (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        const comparehash = await bcrypt.compare(password, user.password);
+        if (!comparehash) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        return done(null, user);
+      });
     })
   );
 };
