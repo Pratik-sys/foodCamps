e0dda62a71b9d436d03db93796602de288435d06
Pratik-sys <pithore.pratik@gmail.com>
2022-01-09 16:57:57 +0530

    refactor middlewares

---
+++ middleware/Auth.js
@@ -0,0 +1,18 @@
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash("error", "Please Sign in to do that!!");
+  res.redirect("/login");
+};
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.send(
+        "<script>function Redirect(){window.location='/foodgrounds';} alert('Unautorize access you will be redirected to foodgrounds in 5 seconds'); setTimeout(Redirect(),5000);</script>"
+      );
+    }
+    next();
+  };
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,19 +1,6 @@
+const { isLoggedIn, authorize } = require("./Auth");
+
 module.exports = {
-  isLoggedIn: (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    req.flash("error", "Please Sign in to do that!!");
-    res.redirect("/login");
-  },
-  authorize: (...roles) => {
-    return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return res.send(
-          "<script>function Redirect(){window.location='/foodgrounds';} alert('Unautorize access you will be redirected to foodgrounds in 5 seconds'); setTimeout(Redirect(),5000);</script>"
-        );
-      }
-      next();
-    };
-  },
+  isLoggedIn,
+  authorize,
 };
