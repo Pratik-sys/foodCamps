23f782f2bbfe4f3e4cec5918ab57ed825c860d00
Pratik-sys <pithore.pratik@gmail.com>
2022-01-09 16:53:06 +0530

    at maxAge to session & refactor

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const app = express();
-
-const methodOveride = require("method-override");
-const config = require("./config");
 const passport = require("passport");
+const app = express();
+require("dotenv").config();
+const config = require("./config");
+const { GenAdmin } = require("./utils");
+const { User } = require("./models");
+const methodOveride = require("method-override");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
@@ -11,38 +13,38 @@ const commentRoutes = require("./routes/comments");
 const foodgroundRoutes = require("./routes/foodgrounds");
 const adminRoutes = require("./routes/Admin");
 const indexRoutes = require("./routes/index");
-const { User } = require("./models");
-const { GenAdmin } = require("./utils/CreateAdmin");
 
-require("dotenv").config();
-require("./config/passport")(passport);
-config.DBConfig();
+config.DB();
 config.CloudinaryConfig();
+
+config.Passport(passport);
+
+app.use(GenAdmin);
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(flash());
+
 app.use(cookieParser("secret"));
-app.use(GenAdmin);
-//require moment
+
 app.locals.moment = require("moment");
-// seedDB(); //seed the database
 
 app.use(methodOveride("_method"));
-// PASSPORT CONFIGURATION
+
 app.use(
   session({
     secret: process.env.SECRETE_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      expires: 600000,
+      maxAge: 600000,
     },
   })
 );
-
-app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -53,7 +55,7 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
